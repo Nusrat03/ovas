d8c7d0386b10795540effd567a31ab655141078a
nusrat03 <maishanusrat101@gmail.com>
2023-05-22 20:48:58 +0600

    first commit

---
+++ admin/home.php
@@ -0,0 +1,18 @@
+<style>
+    #cover-img{
+        object-fit:cover;
+        object-position:center center;
+        width: 100%;
+        height: 100%;
+    }
+    .fc-event-title-container{
+        text-align:center;
+    }
+    .fc-event-title.fc-sticky{
+        font-size:2em;
+    }
+</style>
+
+<h1>Welcome to <?php echo $_settings->info('name') ?> - Admin Panel</h1>
+<hr class="border-info">
+<hr>
